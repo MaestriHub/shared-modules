c9fe7ec5899f529e3666ab5e81d76271e2e23e0a
Vitalii Shevtsov <Vitalik.Shevtsov13@gmail.com>
2024-03-23 00:59:34 -0700

    fix: Добавил Device

---
+++ DTOs/Objects/Device.swift
@@ -0,0 +1,52 @@
+//
+//  Device.swift
+//  Created by Vitalii Shevtsov on 3/22/24.
+//
+
+import Foundation
+
+/// Пространство имен `Device` содержит типы данных для работы с информацией о клиентах.
+///
+/// В него входят  модели ответов (`Responses`),
+/// используемые для обмена данными между клиентом и сервером в контексте клиентских данных.
+public enum Device {
+    public enum Parameters {}
+    public enum Responses {}
+}
+
+public extension Device.Parameters {
+    
+    struct Create: Parametable {
+        
+        public init() {}
+    }
+    
+    struct RegisterPush: Parametable {
+        public enum Token: Codable {
+            case apple(token: String)
+            case fcm(token: String)
+        }
+        
+        public let token: Token
+        
+        public init(token: Token) {
+            self.token = token
+        }
+    }
+}
+
+// MARK: - Responses -
+
+public extension Device.Responses {
+    
+    struct Full: Responsable, Identifiable, Equatable {
+        public var id: UUID
+        
+        public init(
+            id: UUID
+        ) {
+            self.id = id
+        }
+    }
+}
+
