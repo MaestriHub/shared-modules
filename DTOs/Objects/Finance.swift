611764217810faf76104f0dd56d793809dbc7ba4
MeT3ger <pudgekrut4444@yandex.ru>
2024-07-10 21:37:43 +0300

    add finance

---
+++ DTOs/Objects/Finance.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+/// Пространство имен `Appointment` содержит типы данных для работы с записями на прием.
+///
+/// В него входят как параметры для запросов (`Parameters`), так и модели ответов (`Responses`),
+/// которые используются для сериализации данных, отправляемых и получаемых от API.
+public enum Finance {
+    public enum Parameters {}
+    public enum Responses {}
+}
+
+//MARK: - Parameters -
+
+public extension Finance.Parameters {
+    struct Create: Parametable {
+        public var price: Price
+        public var info: [String : String]
+    }
+
+    struct Patch: Parametable {
+        public var price: Price?
+        public var info: [String : String]?
+    }
+}
+
+
+//MARK: - Responses -
+
+public extension Finance.Responses {
+    struct Full: Responsable, Identifiable, Hashable, Equatable {
+        public var id: UUID
+    }
+}
