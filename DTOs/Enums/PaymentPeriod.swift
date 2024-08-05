b77c99e7b0f082993a94d31daa7d7748d893d800
Алексей Иванов <ivanovad97@gmail.com>
2024-08-05 16:08:42 +0500

    update salary

---
+++ DTOs/Enums/PaymentPeriod.swift
@@ -0,0 +1,12 @@
+//
+//  PaymentPeriod.swift
+//  Created by Алексей on 05.08.2024.
+//
+
+import Foundation
+
+public enum PaymentPeriod: Codable, Hashable {
+    case day
+    case month
+    case hour
+}
---
+++ DTOs/Objects/Salary.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+/// Пространство имен `Salary` содержит типы данных для настройки зарплаты для должности.
+///
+/// В него входят как параметры для запросов (`Parameters`), так и модели ответов (`Responses`),
+/// которые используются для сериализации данных, отправляемых и получаемых от API.
+public enum Salary {
+    public enum Parameters {}
+    public enum Responses {}
+}
+
+// MARK: - Parameters -
+
+public extension Salary.Parameters {
+    
+    /// Параметры для создания контакта.
+    /// Включает в себя информацию, необходимую для регистрации нового контактного метода.
+    ///
+    /// ### Properties:
+    /// - `value`: Значение контактного данных, например номер телефона или адрес электронной почты.
+    /// - `type`: Тип контактного метода, определенный в ``ContactType``.
+    struct Create: Parametable {
+        public var procent: Int?
+        public var grid: [UUID : PaymentType]?
+        public var wage: Wage?
+        
+        public init(
+            procent: Int?,
+            grid: [UUID : PaymentType]?,
+            wage: Wage?
+        ) {
+            self.procent = procent
+            self.grid = grid
+            self.wage = wage
+        }
+    }
+}
+
+// MARK: - Responses -
+
+public extension Salary.Responses {
+    
+    /// Полная информация о зарплате.
+    ///
+    /// ### Properties:
+    /// - `id`: Уникальный идентификатор контакта.
+    /// - `procent`: Процент от прибыли.
+    /// - `grid`: Сдельная оплата за конкретную услугу.
+    /// - `wage`: Оклад(за смену, почасовой, ежемесячный).
+    struct Full: Responsable, Identifiable, Hashable, Equatable {
+        public var id: UUID
+        public var procent: Int?
+        public var grid: [Service.Responses.Partial : PaymentType]?
+        public var wage: Wage?
+        
+        public init(
+            id: UUID,
+            procent: Int?,
+            grid: [Service.Responses.Partial : PaymentType]?,
+            wage: Wage?
+        ) {
+            self.id = id
+            self.procent = procent
+            self.grid = grid
+            self.wage = wage
+        }
+    }
+    
+    enum PaymentType: Codable, Hashable {
+        case procent(procent: Int)
+        case value(value: Price)
+    }
+}
