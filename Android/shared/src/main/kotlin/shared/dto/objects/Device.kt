42fff0c31218f4be5b248758b0ceb54c9441071d
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-11-10 19:44:37 +0300

    добавил

--- Android/shared/src/main/kotlin/shared/dto/objects/Device.kt
+++ Android/shared/src/main/kotlin/shared/dto/objects/Device.kt
@@ -8,39 +8,29 @@ import com.maestri.sdk.sources.shared.serializers.UUIDSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
+import shared.dto.enums.SystemType
+import shared.dto.enums.VersionType
 import java.util.UUID
+import javax.print.attribute.standard.PrinterMoreInfoManufacturer
 
 object Device {
     data object Parameters {
-
         @Serializable
-        class Create : Parametable()
-
-        @Serializable
-        data class RegisterPush(
-            val token: Token,
-        ) : Parametable() {
-            @Serializable
-            sealed class Token {
-                @Serializable
-                @SerialName("apple")
-                data class Apple(
-                    val token: String,
-                ) : Token()
-
-                @Serializable
-                @SerialName("fcm")
-                data class Fcm(
-                    val token: String,
-                ) : Token()
-            }
-        }
+        data class System(
+            val manufacturer: String?,
+            val model: String?,
+            val system: SystemType?,
+            val appVersion: String?,
+            val country: String?,
+            val fcmToken: String?,
+        ) : Parametable()
     }
 
     data object Responses {
         @Serializable
         data class Full(
             val id: UUID,
+            val version: VersionType?,
         ) : Responsable
     }
 }
