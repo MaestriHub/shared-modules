19ab60e10e3ea69c46e72f49098d0c23ed94189a
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-11-15 16:03:59 +0300

    чуть-чуть причесал auth router

--- Node/src/api/services/AuthService.ts
+++ Node/src/api/services/AuthService.ts
@@ -1,9 +1,6 @@
 import { client } from "../clientFactory/Client";
 import * as Auth from "../../dto/objects/Auth"
 
-interface Response {}
-interface Request {}
-
 enum Paths {
   GoogleAuth = "auth/google",
   AppleAuth = "auth/apple",
@@ -11,53 +8,26 @@ enum Paths {
 }
 
 export class AuthService {
-  static async GoogleAuth(someValues): Promise<{ error: boolean; data: Auth.Full }> {
-    const specificHeaders = {}
-    const someBody: Auth.GoogleToken = someValues.someBody
-    const someQuery: Request = ""
-
+  static async GoogleAuth(body: Auth.GoogleToken): Promise<Auth.Full> {
     const response = await client.post(Paths.GoogleAuth, {
-      headers: specificHeaders,
-      params: someQuery,
-      data: someBody,
+      data: body
     })
 
-    const resultDTO: Auth.Full = response.data;
-
-    return { error: false, data: resultDTO };
+    return response.data;
   }
 
-  static async AppleAuth(someValues): Promise<{ error: boolean; data: Auth.Full }> {
-    const specificHeaders = {}
-    const someBody: Auth.AppleToken = someValues
-    const someQuery: Request = ""
-
+  static async AppleAuth(body: Auth.AppleToken): Promise<Auth.Full> {
     const response = await client.post(Paths.AppleAuth, {
-      headers: specificHeaders,
-      params: someQuery,
-      data: someBody,
+      data: body,
     })
 
-    const resultDTO: Auth.Full = response.data;
-
-    return { error: false, data: resultDTO };
+    return response.data;
   }
 
-  static async TestAuth(someValues): Promise<{ error: boolean; data: Auth.Full }> {
-    const specificHeaders = {}
-    const someBody: Request = ""
-    const someQuery: Request = ""
+  static async TestAuth(testToken: string): Promise<Auth.Full> {
+    const customUrl = Paths.TestAuth + testToken
+    const response = await client.post(customUrl)
 
-    const customUrl = Paths.TestAuth + someValues.testToken 
-
-    const response = await client.post(customUrl, {
-      headers: specificHeaders,
-      params: someQuery,
-      data: someBody,
-    })
-
-    const resultDTO: Auth.Full = response.data;
-
-    return { error: false, data: resultDTO };
+    return response.data
   }
 }
