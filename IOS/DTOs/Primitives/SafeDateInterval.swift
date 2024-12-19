f5638f89561fedbaaf8adea550d3a0a6ed101f09
Snikem <antong.21072003@gmail.com>
2024-12-19 13:58:01 +0300

    тест, добаил свой интервал

---
+++ IOS/DTOs/Primitives/SafeDateInterval.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+public struct SafeDateInterval: Codable {
+    public var interval: DateInterval
+
+    enum CodingKeys: String, CodingKey {
+        case duration = "duration"
+        case start = "start"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        guard let start = try? container.decode(Int.self, forKey: .start) else {
+             throw DecodingError.dataCorruptedError(
+                forKey: CodingKeys.start,
+                in: container,
+                debugDescription: "start is nil"
+            )
+        }
+        guard let duration = try? container.decode(Int.self, forKey: .duration) else {
+             throw DecodingError.dataCorruptedError(
+                forKey: CodingKeys.duration,
+                in: container,
+                debugDescription: "duration is nil"
+            )
+        }
+        guard duration >= 0 else {
+             throw DecodingError.dataCorruptedError(
+                forKey: CodingKeys.duration,
+                in: container,
+                debugDescription: "duration is negative"
+            )
+        }
+        self.interval = DateInterval(start: Date(timeIntervalSince1970: Double(start)), duration: TimeInterval(duration))
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(interval.start.timeIntervalSince1970, forKey: .start)
+        try container.encode(interval.duration, forKey: .duration)
+    }
+}
