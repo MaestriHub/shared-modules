6c46c6194db9ecfedacb96cb5cdd117530f830e3
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-01-16 22:46:56 +0300

    убрал еще протоколов

--- IOS/DTOs/Enums/ProfessionalEmployee.swift
+++ IOS/DTOs/Enums/ProfessionalEmployee.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum ProfessionalEmployee: Responsable, Equatable {
+public enum ProfessionalEmployee: Responsable {
     case link(URL)
     case value(Professional.Responses.Partial)
     
