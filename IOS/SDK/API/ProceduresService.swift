0f11f881f6449b492ff6e39b0835882c72e21f67
Алексей Иванов <ivanovad97@gmail.com>
2025-02-03 11:58:58 +0700

    fix

--- IOS/SDK/API/ProceduresService.swift
+++ IOS/SDK/API/ProceduresService.swift
@@ -152,8 +152,8 @@ public final class ProceduresServiceMock {
                 price: Price(amount: Decimal(1000 + index * 500), currency: "USD"), // Разные цены
                 alias: "procedure_\(index)",
                 description: "Описание процедуры \(index + 1)",
-                serviceId: services[amount].id,
-                masterId: masters[amount].id
+                serviceId: services[amount - 1].id,
+                masterId: masters[amount - 1].id
             )
         }
     }
