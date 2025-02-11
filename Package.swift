41bbac86b6f12bda88e790e450434b12c0b55764
Vitalii Shevtsov <Vitalik.Shevtsov13@gmail.com>
2025-02-11 00:56:31 -0500

    fix: обидно но мы не готовы пока что бы SDK лежал

--- Package.swift
+++ Package.swift
@@ -10,34 +10,13 @@ let package = Package(
         .library(
             name: "DTOs",
             targets: ["DTOs"]
-        ),
-        .library(
-            name: "MaestriSDK",
-            targets: ["MaestriSDK"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.10.1")),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", .upToNextMajor(from: "1.5.2")),
-        .package(url: "https://github.com/pointfreeco/swift-sharing", .upToNextMajor(from: "2.2.0")),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", .upToNextMajor(from: "4.2.2")),
+        )
     ],
     targets: [
         .target(
             name: "DTOs",
             dependencies: [],
             path: "IOS/DTOs"
-        ),
-        .target(
-            name: "MaestriSDK",
-            dependencies: [
-                "DTOs",
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "Sharing", package: "swift-sharing"),
-                .product(name: "Dependencies", package: "swift-dependencies"),
-                .product(name: "KeychainAccess", package: "KeychainAccess"),
-            ],
-            path: "IOS/SDK"
         ),
     ]
 )
