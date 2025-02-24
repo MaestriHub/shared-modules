2b9f02ebbe5d2facf71b178b3bc7e5c86e31dc53
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-02-24 16:54:03 +0300

    добавил init

--- IOS/DTOs/Objects/AppointmentCustomer.swift
+++ IOS/DTOs/Objects/AppointmentCustomer.swift
@@ -205,6 +205,16 @@ public extension AppointmentCustomer.Responses.Helpers {
         public var id: UUID
         public var nick: String
         public var avatar: URL?
+        
+        public init(
+            id: UUID,
+            nick: String,
+            avatar: URL? = nil
+        ) {
+            self.id = id
+            self.nick = nick
+            self.avatar = avatar
+        }
     }
 }
 
