2680928c4ad952a4904f0ae59a563820bc3ff345
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-03-01 12:37:38 +0300

    пробуем так

---
+++ IOS/DTOs/Objects/MaestriContacts/ContactV2.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+public enum ContactV2 {
+    public enum Parameters {}
+    public enum Responses {}
+}
+
+public extension ContactV2.Parameters {
+    
+    // верификации пока нет потому что не понятно как она будет выглядеть
+    struct Create: Parametable {
+        public let value: String
+        public let type: ContactType
+        
+        public init(
+            value: String,
+            type: ContactType
+        ) {
+            self.value = value
+            self.type = type
+        }
+    }
+    
+    struct Get: Parametable {
+        public let consumer: ContactV2.Helpers.ContactConsumer
+    }
+}
+
+// MARK: - Responses -
+
+public extension ContactV2.Responses {
+
+    struct Contact: Responsable {
+        public var id: UUID
+        public var value: String
+        public var type: ContactType
+        
+        public init(
+            id: UUID,
+            value: String,
+            type: ContactType
+        ) {
+            self.id = id
+            self.value = value
+            self.type = type
+        }
+    }
+    
+    struct All: Responsable {
+        public let allContacts: [Contact]
+    }
+}
+
+public extension ContactV2 {
+    enum Helpers {
+        public enum ContactConsumer: Codable {
+            case user(UUID)
+            case salon(UUID)
+            case customer(UUID)
+            case employee(UUID)
+        }
+    }
+}
