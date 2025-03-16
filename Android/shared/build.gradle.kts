03c58434701ed96f7cb6ea382332af274b38dbf7
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-03-16 18:26:24 +0300

    подправил версию андройда

--- Android/shared/build.gradle.kts
+++ Android/shared/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         register<MavenPublication>("gpr") {
             from(components["kotlin"])
             groupId = "app.maestri"
-            version = "1.1.41"
+            version = "1.1.42"
             artifactId = "shared"
         }
     }
