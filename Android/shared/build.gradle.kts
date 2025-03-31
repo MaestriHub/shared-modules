6b1f9d7080e6eca65da70657b5656eceba182e93
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-03-31 15:15:11 +0300

    Change android version

--- Android/shared/build.gradle.kts
+++ Android/shared/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         register<MavenPublication>("gpr") {
             from(components["kotlin"])
             groupId = "app.maestri"
-            version = "1.1.47"
+            version = "1.1.48"
             artifactId = "shared"
         }
     }
