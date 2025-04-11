e69d196804ba3d86113e62a54b8b4e42ccdaf3d5
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-04-11 22:45:38 +0300

    Refactoring HandleInvite

---
+++ IOS/DTOs/Objects/Common/HandleInvite.swift
@@ -0,0 +1,50 @@
+import Foundation
+import MemberwiseInit
+
+public enum HandleInvite {
+    public enum Parameters {}
+    public enum Responses {}
+    public enum Internal {}
+}
+
+public extension HandleInvite.Parameters {
+  
+    @MemberwiseInit(.public, _optionalsDefaultNil: true)
+    struct Customer: Parametable {
+        public var contactValue: String?
+    }
+    
+    // struct Employee: Parametable
+    // Nothing!
+}
+
+public extension HandleInvite.Responses {
+
+    @MemberwiseInit(.public, _optionalsDefaultNil: true)
+    struct EmployeeSuccess: Responsable {
+        public let id: UUID
+        public let nickname: String
+        public let logo: URL?
+        public let position: String
+    }
+    
+    @MemberwiseInit(.public, _optionalsDefaultNil: true)
+    struct CustomerSuccessOneOf: Responsable {
+        public let verify: HandleInvite.Internal.Verify?
+        public let client: HandleInvite.Internal.Client?
+    }
+}
+
+public extension HandleInvite.Internal {
+    
+    @MemberwiseInit(.public, _optionalsDefaultNil: true)
+    struct Verify: Responsable {
+        public let contacts: [Contact.Shared.PrimaryContact]
+    }
+    
+    @MemberwiseInit(.public, _optionalsDefaultNil: true)
+    struct Client: Responsable {
+        public let alias: String
+    }
+}
+
