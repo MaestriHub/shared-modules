3e0ef339afa587c45bf32ca9aa10b3ab8858e543
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-05-01 20:05:30 +0300

    creds v2

--- IOS/DTOs/OptionSet/PermissionSet.swift
+++ IOS/DTOs/OptionSet/PermissionSet.swift
@@ -1,90 +1,102 @@
-public struct CredentialsSet: OptionSet, Codable, Hashable {
-    public var rawValue: Int
-    public init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
+public struct CredentialsSet: Codable, Hashable {
+    public var clientCreds: Set<ClientCredentials>
+    public var salonCreds: Set<SalonCredentials>
+    public var employeeCreds: Set<EmployeeCredentials>
+    public var procedureCreds: Set<ProcedureCredentials>
+    public var positionCreds: Set<PositionCredentials>
+    public var worktimeCreds: Set<WorktimeCredentials>
+    public var appointmentCreds: Set<AppointmentCredentials>
+    public var statisticCreds: Set<StatisticCredentials>
+    public var notifyCreds: Set<NotificationCredentials>
+    public var financeCreds: Set<FinanceCredentials>
+    public var salaryCreds: Set<SalaryCredentials>
     
-    public enum Client {
-        public static let edit = CredentialsSet(rawValue: 0)
-        public static let visibleBaseInfo = CredentialsSet(rawValue: 1)
-        public static let visibleContacts = CredentialsSet(rawValue: 2)
+    public enum ClientCredentials: Codable {
+        case edit
+        case visibleBaseInfo
+        case visibleContacts
     }
-    
-    public enum Salon {
-        public static let editBaseInfo = CredentialsSet(rawValue: 3)
+
+    public enum SalonCredentials: Codable {
+        case editBaseInfo
     }
-    
-    public enum Employee {
-        public static let edit = CredentialsSet(rawValue: 4)
-        public static let visible = CredentialsSet(rawValue: 5)
+
+    public enum EmployeeCredentials: Codable {
+        case edit
+        case visible
     }
-    
-    public enum Procedure {
-        public static let ownCreate = CredentialsSet(rawValue: 6)
-        public static let otherCreate = CredentialsSet(rawValue: 7)
+
+    public enum ProcedureCredentials: Codable {
+        case ownCreate
+        case otherCreate
     }
-    
-    public enum Position {
-        public static let edit = CredentialsSet(rawValue: 8)
+
+    public enum PositionCredentials: Codable {
+        case edit
     }
-    
-    public enum Worktime {
-        public static let ownEdit = CredentialsSet(rawValue: 9)
-        public static let otherEdit = CredentialsSet(rawValue: 10)
+
+    public enum WorktimeCredentials: Codable {
+        case ownEdit
+        case otherEdit
     }
-    
+
     /// ----------------------
-    
-    public enum Appointment {
-        public static let all = CredentialsSet(rawValue: 11)
+
+    public enum AppointmentCredentials: Codable {
+       case all
     }
-    
-    public enum Statistic {
-        public static let all = CredentialsSet(rawValue: 12)
+
+    public enum StatisticCredentials: Codable {
+        case all
     }
-    
-    public enum Notification {
-        public static let all = CredentialsSet(rawValue: 13)
+
+    public enum NotificationCredentials: Codable {
+        case all
     }
-    
-    public enum Finance {
-        public static let all = CredentialsSet(rawValue: 14)
+
+    public enum FinanceCredentials: Codable {
+        case all
     }
-    
-    public enum Salary {
-        public static let all = CredentialsSet(rawValue: 15)
+
+    public enum SalaryCredentials: Codable {
+        case all
     }
 }
 
 public extension CredentialsSet {
-    static let none: CredentialsSet = []
+    mutating func insert(c: ClientCredentials)       { self.clientCreds.insert(c) }
+    mutating func insert(c: SalonCredentials)        { self.salonCreds.insert(c) }
+    mutating func insert(c: EmployeeCredentials)     { self.employeeCreds.insert(c) }
+    mutating func insert(c: ProcedureCredentials)    { self.procedureCreds.insert(c) }
+    mutating func insert(c: PositionCredentials)     { self.positionCreds.insert(c) }
+    mutating func insert(c: WorktimeCredentials)     { self.worktimeCreds.insert(c) }
+    mutating func insert(c: AppointmentCredentials)  { self.appointmentCreds.insert(c) }
+    mutating func insert(c: StatisticCredentials)    { self.statisticCreds.insert(c) }
+    mutating func insert(c: NotificationCredentials) { self.notifyCreds.insert(c) }
+    mutating func insert(c: SalaryCredentials)       { self.salaryCreds.insert(c) }
+    mutating func insert(c: FinanceCredentials)      { self.financeCreds.insert(c) }
     
-    static let all: CredentialsSet = [
-        Client.edit,
-        Client.visibleBaseInfo,
-        Client.visibleContacts,
-        
-        Salon.editBaseInfo,
-        
-        Employee.edit,
-        Employee.visible,
-        
-        Procedure.otherCreate,
-        Procedure.ownCreate,
-        
-        Position.edit,
-        
-        Worktime.otherEdit,
-        Worktime.ownEdit,
-        
-        Appointment.all,
-        
-        Statistic.all,
-        
-        Notification.all,
-        
-        Finance.all,
-        
-        Salary.all,
-    ]
+    mutating func remove(c: ClientCredentials)       { self.clientCreds.remove(c) }
+    mutating func remove(c: SalonCredentials)        { self.salonCreds.remove(c) }
+    mutating func remove(c: EmployeeCredentials)     { self.employeeCreds.remove(c) }
+    mutating func remove(c: ProcedureCredentials)    { self.procedureCreds.remove(c) }
+    mutating func remove(c: PositionCredentials)     { self.positionCreds.remove(c) }
+    mutating func remove(c: WorktimeCredentials)     { self.worktimeCreds.remove(c) }
+    mutating func remove(c: AppointmentCredentials)  { self.appointmentCreds.remove(c) }
+    mutating func remove(c: StatisticCredentials)    { self.statisticCreds.remove(c) }
+    mutating func remove(c: NotificationCredentials) { self.notifyCreds.remove(c) }
+    mutating func remove(c: SalaryCredentials)       { self.salaryCreds.remove(c) }
+    mutating func remove(c: FinanceCredentials)      { self.financeCreds.remove(c) }
+    
+    mutating func contains(c: ClientCredentials)       -> Bool { self.clientCreds.contains(c) }
+    mutating func contains(c: SalonCredentials)        -> Bool { self.salonCreds.contains(c) }
+    mutating func contains(c: EmployeeCredentials)     -> Bool { self.employeeCreds.contains(c) }
+    mutating func contains(c: ProcedureCredentials)    -> Bool { self.procedureCreds.contains(c) }
+    mutating func contains(c: PositionCredentials)     -> Bool { self.positionCreds.contains(c) }
+    mutating func contains(c: WorktimeCredentials)     -> Bool { self.worktimeCreds.contains(c) }
+    mutating func contains(c: AppointmentCredentials)  -> Bool { self.appointmentCreds.contains(c) }
+    mutating func contains(c: StatisticCredentials)    -> Bool { self.statisticCreds.contains(c) }
+    mutating func contains(c: NotificationCredentials) -> Bool { self.notifyCreds.contains(c) }
+    mutating func contains(c: SalaryCredentials)       -> Bool { self.salaryCreds.contains(c) }
+    mutating func contains(c: FinanceCredentials)      -> Bool { self.financeCreds.contains(c) }
 }
