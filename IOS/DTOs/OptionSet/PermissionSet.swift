1575b81f36ab9e6746a35e7551bfa9f9e23d9f40
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-05-01 19:36:21 +0300

    make public

--- IOS/DTOs/OptionSet/PermissionSet.swift
+++ IOS/DTOs/OptionSet/PermissionSet.swift
@@ -5,54 +5,54 @@ public struct CredentialsSet: OptionSet, Codable, Hashable {
     }
     
     public enum Client {
-        static let edit = CredentialsSet(rawValue: 0)
-        static let visibleBaseInfo = CredentialsSet(rawValue: 1)
-        static let visibleContacts = CredentialsSet(rawValue: 2)
+        public static let edit = CredentialsSet(rawValue: 0)
+        public static let visibleBaseInfo = CredentialsSet(rawValue: 1)
+        public static let visibleContacts = CredentialsSet(rawValue: 2)
     }
     
     public enum Salon {
-        static let editBaseInfo = CredentialsSet(rawValue: 3)
+        public static let editBaseInfo = CredentialsSet(rawValue: 3)
     }
     
     public enum Employee {
-        static let edit = CredentialsSet(rawValue: 4)
-        static let visible = CredentialsSet(rawValue: 5)
+        public static let edit = CredentialsSet(rawValue: 4)
+        public static let visible = CredentialsSet(rawValue: 5)
     }
     
     public enum Procedure {
-        static let ownCreate = CredentialsSet(rawValue: 6)
-        static let otherCreate = CredentialsSet(rawValue: 7)
+        public static let ownCreate = CredentialsSet(rawValue: 6)
+        public static let otherCreate = CredentialsSet(rawValue: 7)
     }
     
     public enum Position {
-        static let edit = CredentialsSet(rawValue: 8)
+        public static let edit = CredentialsSet(rawValue: 8)
     }
     
     public enum Worktime {
-        static let ownEdit = CredentialsSet(rawValue: 9)
-        static let otherEdit = CredentialsSet(rawValue: 10)
+        public static let ownEdit = CredentialsSet(rawValue: 9)
+        public static let otherEdit = CredentialsSet(rawValue: 10)
     }
     
     /// ----------------------
     
     public enum Appointment {
-        static let all = CredentialsSet(rawValue: 11)
+        public static let all = CredentialsSet(rawValue: 11)
     }
     
     public enum Statistic {
-        static let all = CredentialsSet(rawValue: 12)
+        public static let all = CredentialsSet(rawValue: 12)
     }
     
     public enum Notification {
-        static let all = CredentialsSet(rawValue: 13)
+        public static let all = CredentialsSet(rawValue: 13)
     }
     
     public enum Finance {
-        static let all = CredentialsSet(rawValue: 14)
+        public static let all = CredentialsSet(rawValue: 14)
     }
     
     public enum Salary {
-        static let all = CredentialsSet(rawValue: 15)
+        public static let all = CredentialsSet(rawValue: 15)
     }
 }
 
