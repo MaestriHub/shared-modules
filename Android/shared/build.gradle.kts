d8d27cf507d89d5adbf6a182839df8287c84e8d1
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-05-19 23:44:30 +0300

    Update build.gradle.kts

--- Android/shared/build.gradle.kts
+++ Android/shared/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         register<MavenPublication>("gpr") {
             from(components["kotlin"])
             groupId = "app.maestri"
-            version = "1.1.54"
+            version = "1.1.55"
             artifactId = "shared"
         }
     }
