12bdb37b3bce1a05c6c6a6009aef1f1a5598b621
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-06-13 14:45:22 +0300

    add docs

--- IOS/DTOs/Helpers/SafeDateInterval+Helpers.swift
+++ IOS/DTOs/Helpers/SafeDateInterval+Helpers.swift
@@ -1,16 +1,30 @@
 import Foundation
 
+
 public extension SafeDateInterval {
+    
+    ///     13:00      14:00
+    /// input:   |----------|          c: .hour(2)
+    ///
+    ///     15:00      16:00
+    /// output: |---------|
+    ///
     @discardableResult
-    func plus(_ c: TimeComponent) -> SafeDateInterval {
+    func rightShift(_ c: TimeComponent) -> SafeDateInterval {
         let newEnd = end.plus(c)
         let newStart = start.plus(c)
         
         return SafeDateInterval(dateinterval: .init(start: newStart, end: newEnd))
     }
 
+    ///     13:00      14:00
+    /// input:   |----------|          c: .hour(2)
+    ///
+    ///     11:00      12:00
+    /// output: |---------|
+    ///
     @discardableResult
-    func minus(_ c: TimeComponent) -> SafeDateInterval  {
+    func leftShift(_ c: TimeComponent) -> SafeDateInterval  {
         let newStart = start.minus(c)
         let newEnd = end.minus(c)
         
