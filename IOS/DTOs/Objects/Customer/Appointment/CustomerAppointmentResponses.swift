e83264a5c09514618e91405d1c2692e36644f2d2
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-06-18 20:12:17 +0300

    добавил недостающих полей сейчас буду править бек

--- IOS/DTOs/Objects/Customer/Appointment/CustomerAppointmentResponses.swift
+++ IOS/DTOs/Objects/Customer/Appointment/CustomerAppointmentResponses.swift
@@ -7,12 +7,17 @@ public extension CustomerAPI.Appointment.Responses {
         public var id: UUID
         public var title: String
         public var price: Decimal
-        public var clientId: UUID
-        public var clientName: String
         public var currency: String
         public var time: SafeDateInterval
         public var procedureId: UUID
+        public var salonLogo: URL
+        public var salonId: UUID
+        public var point: CoordinatePoint
+        public var address: Address
         public var employeeId: UUID
+        public var employeeName: String
+        public var employeeAvatar: URL
+        public var employeeContacts: [Contact.Shared.PrimaryContact]
     }
     
     @MemberwiseInit(.public)
@@ -20,9 +25,11 @@ public extension CustomerAPI.Appointment.Responses {
         public var id: UUID
         public var alias: String?
         public var price: Decimal
-        public var clientId: UUID
-        public var clientName: String
         public var currency: String
+        public var salonLogo: URL
+        public var salonId: UUID
+        public var point: CoordinatePoint
+        public var address: Address
         public var procedures: [Procedure]
         
         @MemberwiseInit(.public)
@@ -30,6 +37,9 @@ public extension CustomerAPI.Appointment.Responses {
             public var id: UUID
             public var title: String
             public var employeeId: UUID
+            public var employeeName: String
+            public var employeeAvatar: URL
+            public var employeeContacts: [Contact.Shared.PrimaryContact]
             public var time: SafeDateInterval
         }
     }
--- IOS/DTOs/Objects/Employee/Appointment/EmployeeAppointmentResponses.swift
+++ IOS/DTOs/Objects/Employee/Appointment/EmployeeAppointmentResponses.swift
@@ -17,10 +17,19 @@ public extension EmployeeAPI.Appointment.Responses {
         public var price: Decimal
         public var clientId: UUID
         public var clientName: String
+        public var clientAvatar: URL
+        public var clientContacts: [Contact.Shared.PrimaryContact]
         public var currency: String
         public var time: SafeDateInterval
         public var procedureId: UUID
+        public var salonLogo: URL
+        public var salonId: UUID
+        public var point: CoordinatePoint
+        public var address: Address
         public var employeeId: UUID
+        public var employeeName: String
+        public var employeeAvatar: URL
+        public var employeeContacts: [Contact.Shared.PrimaryContact]
     }
     
     @MemberwiseInit(.public)
@@ -28,17 +37,26 @@ public extension EmployeeAPI.Appointment.Responses {
         public var id: UUID
         public var alias: String?
         public var price: Decimal
+        public var currency: String
         public var clientId: UUID
         public var clientName: String
-        public var currency: String
+        public var clientAvatar: URL
+        public var clientContacts: [Contact.Shared.PrimaryContact]
+        public var salonLogo: URL
+        public var salonId: UUID
+        public var point: CoordinatePoint
+        public var address: Address
         public var procedures: [Procedure]
         
         @MemberwiseInit(.public)
         public struct Procedure: Codable {
             public var id: UUID
             public var title: String
-            public var employeeId: UUID
             public var time: SafeDateInterval
+            public var employeeId: UUID
+            public var employeeName: String
+            public var employeeAvatar: URL
+            public var employeeContacts: [Contact.Shared.PrimaryContact]
         }
     }
 }
