d301d9eb9f82b595cae9c5fde88997748308b7ca
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-06-21 20:04:34 +0300

    add helpers for safeDateInterval

--- IOS/DTOs/Helpers/SafeDateInterval+Helpers.swift
+++ IOS/DTOs/Helpers/SafeDateInterval+Helpers.swift
@@ -38,3 +38,22 @@ public extension SafeDateInterval {
         return true
     }
 }
+
+public extension SafeDateInterval {
+    
+    // check docs for Date
+    func convertFromUTC(to timeZone: TimeZone) throws -> SafeDateInterval {
+        let newStart = start.convertFromUTC(to: timeZone)
+        let newEnd = end.convertFromUTC(to: timeZone)
+        
+        return .init(dateinterval: .init(start: newStart, end: newEnd))
+    }
+
+    // check docs for Date
+    func convertToUTC(from timeZone: TimeZone) throws -> SafeDateInterval {
+        let newStart = start.convertToUTC(from: timeZone)
+        let newEnd = end.convertToUTC(from: timeZone)
+        
+        return .init(dateinterval: .init(start: newStart, end: newEnd))
+    }
+}
