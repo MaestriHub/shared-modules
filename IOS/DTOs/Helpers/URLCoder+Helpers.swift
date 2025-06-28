bfc25dee4def8b3e1dad3418fe9bff316d2b1407
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-06-28 23:54:02 +0300

    обновил нейминг

--- IOS/DTOs/Helpers/URLCoder+Helpers.swift
+++ IOS/DTOs/Helpers/URLCoder+Helpers.swift
@@ -5,11 +5,13 @@ func defaultURLDateEncode() -> (Date) throws -> String? {
 }
 
 func URLDateEncode(_ formatter: ISO8601DateFormatter, _ kDefaultPresicion: Date.RoundingPrecision) -> (Date) -> String? {
+    let kMagicASCIISymbol = "%3A" // as :
+    
     return { date in
         let formattedDate = date.rounded(kDefaultPresicion)
         
         var dateString = formatter.string(from: formattedDate)
-        dateString.replace(":", with: "%3A")
+        dateString.replace(":", with: kMagicASCIISymbol)
         
         return dateString
     }
