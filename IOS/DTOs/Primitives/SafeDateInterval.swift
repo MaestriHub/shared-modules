9af696c8014d7153fb51da99f37e310a296b26a1
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-06-28 23:01:37 +0300

    поменял декодеры для safedateinterval

--- IOS/DTOs/Primitives/SafeDateInterval.swift
+++ IOS/DTOs/Primitives/SafeDateInterval.swift
@@ -29,36 +29,10 @@ public struct SafeDateInterval: Codable, Equatable, Hashable, Comparable {
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let start = try container.decode(Date.self, forKey: .start)
+        let end   = try container.decode(Date.self, forKey: .end)
 
-        let dateFormatter = ISO8601DateFormatter()
-        dateFormatter.formatOptions = [.withInternetDateTime]
-        var start: Date?
-        var end: Date?
-        
-        // Пробуем декодировать start как строку (ISO8601)
-        if let startString = try? container.decode(String.self, forKey: .start) {
-            start = dateFormatter.date(from: startString)
-        }
-        
-        // Пробуем декодировать end как строку (ISO8601)
-        if let endString = try? container.decode(String.self, forKey: .end) {
-            end = dateFormatter.date(from: endString)
-        }
-        
-        guard let start = start else {
-            throw DecodingError.dataCorruptedError(
-                forKey: CodingKeys.start,
-                in: container,
-                debugDescription: "Start date is neither a valid ISO8601 string nor a Unix timestamp"
-            )   
-        }
-        guard let end = end else {
-            throw DecodingError.dataCorruptedError(
-                forKey: CodingKeys.end,
-                in: container,
-                debugDescription: "End date is neither a valid ISO8601 string nor a Unix timestamp"
-            )
-        }
         guard start <= end else {
             throw SafeDateIntervalError(reason: "start > end")
         }
@@ -69,13 +43,8 @@ public struct SafeDateInterval: Codable, Equatable, Hashable, Comparable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
-        let dateFormatter = ISO8601DateFormatter()
-        dateFormatter.formatOptions = [.withInternetDateTime]
-        
-        let startString = dateFormatter.string(from: interval.start)
-        let endString = dateFormatter.string(from: interval.end)
-        try container.encode(startString, forKey: .start)
-        try container.encode(endString, forKey: .end)
+        try container.encode(interval.start, forKey: .start)
+        try container.encode(interval.end, forKey: .end)
     }
 
     public static func < (lhs: SafeDateInterval, rhs: SafeDateInterval) -> Bool {
