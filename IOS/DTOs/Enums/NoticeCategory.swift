b6c8472317045149707a67c171306c91038d9f10
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-08-01 21:00:24 +0300

    добавил марку

--- IOS/DTOs/Enums/NoticeCategory.swift
+++ IOS/DTOs/Enums/NoticeCategory.swift
@@ -1,7 +1,7 @@
 import Foundation
 
+// MARK: titles used for notification. If you want to change naming, keep it.
 public enum NoticeCategory: String, Codable, Hashable, Equatable {
-    
     // for employee
     case appointment
     
