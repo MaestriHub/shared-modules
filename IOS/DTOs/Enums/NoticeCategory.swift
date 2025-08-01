ff3f5babd4abcdc53c3bc5773b60f4df9e839192
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-08-01 21:21:35 +0300

    Revert "notice category"
    This reverts commit 472cdef512baa6e8b1ce662021222f372942431c.

--- IOS/DTOs/Enums/NoticeCategory.swift
+++ IOS/DTOs/Enums/NoticeCategory.swift
@@ -2,13 +2,11 @@ import Foundation
 
 // MARK: titles used for notification. If you want to change naming, keep it.
 public enum NoticeCategory: String, Codable, Hashable, Equatable {
-    public enum Appointment {
-        // for employee
-        case appointment
-        
-        // for client
-        case assigment
-    }
+    // for employee
+    case appointment
+    
+    // for client
+    case assigment
     
     case employee
     case position
