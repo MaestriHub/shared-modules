3dfcc58841eb63feb60133dea17674f124cf3fd5
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2025-09-15 20:38:00 +0300

    Up

--- IOS/DTOs/Objects/Employee/Payments/Subscriptions.swift
+++ IOS/DTOs/Objects/Employee/Payments/Subscriptions.swift
@@ -14,10 +14,18 @@ public struct PaymentSubscriptionResponse: Codable {
     public var monthlyPayURL : String
 }
 
+public enum SubscriptionPeriodType: String, Codable {
+    case year
+    case month
+}
+
 @MemberwiseInit(.public)
 public struct SubscriptionResponse: Codable {
     public var id: UUID
     public var stripeId: String?
+    public var title: String
     public var startDate: Date
     public var endDate: Date
+    public var periodType: SubscriptionPeriodType
+    public var price: Price
 }
