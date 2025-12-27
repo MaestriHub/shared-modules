98ecb52cd890c1d1414bae52c4c219bb106b67b7
Vitalii Shevtsov <Vitalik.Shevtsov13@gmail.com>
2025-12-27 02:38:27 -0500

    Update Complex.swift

--- IOS/DTOs/Objects/Employee/Catalog/Complex.swift
+++ IOS/DTOs/Objects/Employee/Catalog/Complex.swift
@@ -14,58 +14,49 @@ public typealias ComplexChunkId = UUID
 public extension Complex.Parameters {
     
     struct All: Codable, Sendable {
-        public let salonsFilter: [UUID]?
-        public let employeesFilter: [UUID]?
+        public let salonFilter: UUID?
+        public let employeeFilter: UUID?
         public let pagination: Pagination?
         
-        public init(salonsFilter: [UUID]? = nil, employeesFilter: [UUID]? = nil, pagination: Pagination? = nil) {
-            self.salonsFilter = salonsFilter
-            self.employeesFilter = employeesFilter
+        public init(salonFilter: UUID? = nil, employeeFilter: UUID? = nil, pagination: Pagination? = nil) {
+            self.salonFilter = salonFilter
+            self.employeeFilter = employeeFilter
             self.pagination = pagination
         }
     }
-    
-    /// Пока комплекс можно создавать с помощью айдишников процедур, которые я копирую и делаю доступными только в комплексе
-    /// При запросе на все процедуры их не получить, а при запросе по id, их можно получить и подправить. (Если очень хочется).
-    /// Думаю что в будущем добавиться возможность создавать комплекс не с айдишниками процедур, а с его личными
-    /// процедурами, это в том случае, если таких процедур нет как явления, но в комплексе они существуют
+
     struct Create: Codable, Sendable {
         public let alias: String?
         public let description: String?
         public let priceShift: Complex.Helpers.PriceShift
-        public let chunks: [Complex.Helpers.CreateChunkRequest]
+        public let procedures: [Complex.Helpers.CreateProcedure]
         
         public init(
             alias: String? = nil,
             description: String? = nil,
             priceShift: Complex.Helpers.PriceShift,
-            chunks: [Complex.Helpers.CreateChunkRequest]
+            procedures: [Complex.Helpers.CreateProcedure]
         ) {
             self.alias = alias
             self.description = description
             self.priceShift = priceShift
-            self.chunks = chunks
+            self.procedures = procedures
         }
     }
     
     struct Update: Codable, Sendable {
-        public typealias ChunkPosition = Int
-    
+        public let alias: String?
+        public let description: String?
         public let priceShift: Complex.Helpers.PriceShift?
-        public let description: UpdateString?
-        public let alias: UpdateString?
-        public let shuffleChunks: [ComplexChunkId : ChunkPosition]?
         
         public init(
+            alias: String? = nil,
+            description: String? = nil,
             priceShift: Complex.Helpers.PriceShift? = nil,
-            description: UpdateString? = nil,
-            alias: UpdateString? = nil,
-            shuffleChunks: [ComplexChunkId : ChunkPosition]? = nil
         ) {
-            self.priceShift = priceShift
-            self.description = description
             self.alias = alias
-            self.shuffleChunks = shuffleChunks
+            self.description = description
+            self.priceShift = priceShift
         }
     }
 }
@@ -73,91 +64,43 @@ public extension Complex.Parameters {
 // MARK: - Responses -
 
 public extension Complex.Responses {
+
+    typealias Create = Complex.Helpers.ComplexResponse
     
-    struct Create: Codable, Sendable {
-        public let id: UUID
-        public let priceShift: Complex.Helpers.PriceShift
-        public let description: String?
-        public let alias: String?
-        public let chunks: [Complex.Helpers.ChunkResponse]
-        
-        public init(
-            id: UUID,
-            priceShift: Complex.Helpers.PriceShift,
-            description: String? = nil,
-            alias: String? = nil,
-            chunks: [Complex.Helpers.ChunkResponse]
-        ) {
-            self.id = id
-            self.priceShift = priceShift
-            self.description = description
-            self.alias = alias
-            self.chunks = chunks
-        }
-    }
+    typealias All = [Complex.Helpers.ComplexResponse]
+
+    typealias Update = Complex.Helpers.ComplexResponse
     
-    struct Update: Codable, Sendable {
-        public let id: UUID
-        public let alias: String?
-        public let description: String?
-        public let priceShift: Complex.Helpers.PriceShift
-        public let chunks: [Complex.Helpers.ChunkResponse]
-        
-        public init(
-            id: UUID,
-            alias: String? = nil,
-            description: String? = nil,
-            priceShift: Complex.Helpers.PriceShift,
-            chunks: [Complex.Helpers.ChunkResponse]
-        ) {
-            self.id = id
-            self.alias = alias
-            self.description = description
-            self.priceShift = priceShift
-            self.chunks = chunks
-        }
-    }
-    
-    struct All: Codable, Sendable {
-        public let complexes: [Complex.Helpers.ComplexResponse]
-        
-        public init(complexes: [Complex.Helpers.ComplexResponse]) {
-            self.complexes = complexes
-        }
-    }
-    
-    struct Retrieve: Codable, Sendable {
-        public let id: UUID
-        public let alias: String?
-        public let description: String?
-        public let priceShift: Complex.Helpers.PriceShift
-        public let chunks: [Complex.Helpers.ChunkResponse]
-        
-        public init(
-            id: UUID,
-            alias: String? = nil,
-            description: String? = nil,
-            priceShift: Complex.Helpers.PriceShift,
-            chunks: [Complex.Helpers.ChunkResponse]
-        ) {
-            self.id = id
-            self.alias = alias
-            self.description = description
-            self.priceShift = priceShift
-            self.chunks = chunks
-        }
-    }
+    typealias Retrieve = Complex.Helpers.ComplexResponse
 }
 
 public extension Complex.Helpers {
     
-    struct CreateChunkRequest: Codable, Sendable {
+    struct CreateProcedure: Codable, Sendable {
+        public let alias: String?
+        public let description: String?
         public let order: Int
-        public let proceduresIds: [UUID]
+        public let duration: Minutes
+        public let price: Price
+        public let serviceId: UUID
+        public let employeeIds: [UUID]
         
-        public init(order: Int, proceduresIds: [UUID]) {
+        public init(
+            alias: String? = nil,
+            description: String? = nil,
+            order: Int,
+            duration: Minutes,
+            price: Price,
+            serviceId: UUID,
+            employeeIds: [UUID]
+        ) {
+            self.alias = alias
+            self.description = description
             self.order = order
-            self.proceduresIds = proceduresIds
+            self.duration = duration
+            self.price = price
+            self.serviceId = serviceId
+            self.employeeIds = employeeIds
         }
     }
 }
@@ -166,79 +109,87 @@ public extension Complex.Helpers {
     
     struct ComplexResponse: Codable, Sendable {
         public let id: UUID
-        public let alias: String?
+        public let title: String
         public let description: String?
         public let priceShift: PriceShift
-        public let chunks: [ChunkResponse]
+        public let procedures: [ProcedureResponse]
         
         public init(
             id: UUID,
-            alias: String? = nil,
+            title: String,
             description: String? = nil,
             priceShift: PriceShift,
-            chunks: [ChunkResponse]
+            procedures: [ProcedureResponse]
         ) {
             self.id = id
-            self.alias = alias
+            self.title = title
             self.description = description
             self.priceShift = priceShift
-            self.chunks = chunks
-        }
-    }
-    
-    struct ChunkResponse: Codable, Sendable {
-        public let id: UUID
-        public let order: Int
-        public let procedures: [ProcedureResponse]
-        public let serviceId: UUID
-        public let serviceTitle: String
-        public let serviceTags: [TranslatedServiceTag]
-        
-        public init(
-            id: UUID,
-            order: Int,
-            procedures: [ProcedureResponse],
-            serviceId: UUID,
-            serviceTitle: String,
-            serviceTags: [TranslatedServiceTag]
-        ) {
-            self.id = id
-            self.order = order
             self.procedures = procedures
-            self.serviceId = serviceId
-            self.serviceTitle = serviceTitle
-            self.serviceTags = serviceTags
         }
     }
     
+     /// Структура процедуры с массивом executions
     struct ProcedureResponse: Codable, Sendable {
         public let id: UUID
-        public let alias: String?
+        public let title: String
         public let description: String?
-        public let masterId: UUID
-        public let masterNickname: String
-        public let masterAvatar: URL
-        public let duration: Minutes
-        public let price: Price
+        public let serviceId: UUID
+        public let serviceTags: [TranslatedServiceTag]
+        public let serviceTitle: String
+        public let minDuration: Minutes
+        public let minPrice: Price
+        public let currency: String
+        public let executions: [ExecutionResponse]
         
         public init(
             id: UUID,
-            alias: String? = nil,
+            title: String,
             description: String? = nil,
-            masterId: UUID,
-            masterNickname: String,
-            masterAvatar: URL,
-            duration: Minutes,
-            price: Price
+            serviceId: UUID,
+            serviceTags: [TranslatedServiceTag],
+            serviceTitle: String,
+            minDuration: Minutes,
+            minPrice: Price,
+            currency: String,
+            executions: [ExecutionResponse]
         ) {
             self.id = id
-            self.alias = alias
+            self.title = title
             self.description = description
-            self.masterId = masterId
-            self.masterNickname = masterNickname
-            self.masterAvatar = masterAvatar
-            self.duration = duration
+            self.serviceId = serviceId
+            self.serviceTags = serviceTags
+            self.serviceTitle = serviceTitle
+            self.minDuration = minDuration
+            self.minPrice = minPrice
+            self.currency = currency
+            self.executions = executions
+        }
+    }
+    
+    /// Структура execution в процедуре
+    struct ExecutionResponse: Codable, Sendable {
+        public let price: Decimal
+        public let duration: Minutes
+        public let currency: String
+        public let masterId: UUID
+        public let masterName: String
+        public let masterAvatar: URL
+        
+        public init(
+            price: Decimal,
+            duration: Minutes,
+            currency: String,
+            masterId: UUID,
+            masterName: String,
+            masterAvatar: URL
+        ) {
             self.price = price
+            self.duration = duration
+            self.currency = currency
+            self.masterId = masterId
+            self.masterName = masterName
+            self.masterAvatar = masterAvatar
         }
     }
 }
